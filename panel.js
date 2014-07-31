638a75bed7e08deadb0407de23d3552795f845ce
Michaël Krens <mkrens@gmail.com>
2014-07-31 15:14:48 +0200

    somehow the header.name is lowercase
    probably fixes: https://github.com/recamshak/django-debug-panel/issues/7

--- panel.js
+++ panel.js
@@ -47,7 +47,7 @@ function buildRequestEl(requestUrl, debugDataUrl) {
 
 chrome.devtools.network.onRequestFinished.addListener(function(entry) {
 	$.each(entry.response.headers, function(i, header) {
-		if (header.name === 'X-debug-data-url') {
+		if (header.name === 'x-debug-data-url') {
 			$('#request-list').append(buildRequestEl(entry.request.url, header.value));
 			return false;
 		}
